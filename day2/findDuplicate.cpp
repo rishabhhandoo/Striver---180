f2b12577ce1899cdae8e3f519fe03237fcd9159b
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-07-15 21:15:37 +0530

    easy

---
+++ day2/findDuplicate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        if(nums.size()<2) return -1;
+        
+        int slow=nums[0];
+        int fast=nums[nums[0]];
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }
+        
+        fast=0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return fast;
+    }
+};
+
