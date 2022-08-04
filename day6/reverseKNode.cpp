e2b5f5e2542070a281682194bf44115c8dbf07cb
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-08-04 22:01:47 +0530

    hard

---
+++ day6/reverseKNode.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+    
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode* curr=nullptr;
+        ListNode* prev = nullptr;
+        ListNode* nxt =nullptr;
+        
+        ListNode* after = dummy->next;
+        ListNode* before = dummy;
+        
+        while(true){
+            ListNode* cursor = after;
+            
+            for(int i=0;i<k;i++)
+            {
+                if(!cursor)     return dummy->next;
+                cursor=cursor->next;
+            }
+            curr = after;
+            prev = before;
+            
+            for(int i=0;i<k;i++){
+                nxt=curr->next;
+                curr->next=prev;
+                prev = curr;
+                curr=nxt;
+            }
+            after->next=curr;
+            before->next=prev;
+            before = after;
+            after = curr;
+            
+        }
+        
+        
+        
+
+    }
+};
