b5e8ad64061b4518ebf1b92a56335b9f01bbcc42
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-08-07 21:48:56 +0530

    easy

---
+++ day8/NmeetingsOneRoom.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int,int>> slot;
+        
+        for(int i=0;i<n;i++)
+        {
+            slot.push_back({end[i],start[i]});
+        }
+        
+        sort(slot.begin(),slot.end());
+        
+        int prevend=-1;
+        int ans=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(slot[i].second > prevend){
+                prevend=slot[i].first;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
