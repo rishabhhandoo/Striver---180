cf87c4e3298009df071f1881d70af2c436c1be12
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-09-05 11:16:57 +0530

    medium

---
+++ day3/pow_x_n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n;
+        
+        if(n<0) nn=nn*-1;
+        
+        while(nn)
+        {
+            if(nn%2)
+            {
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else
+            {
+                x=x*x;
+                nn=nn/2;
+            }
+        }
+        if(n<0)
+            ans = (double)1.0/(double)ans;
+
+        return ans;
+    }
+};
