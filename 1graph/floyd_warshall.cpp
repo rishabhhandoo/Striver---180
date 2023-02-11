b2c103f1e952ad0af01f0e4f32f95a429ad7b817
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2023-02-11 08:48:34 +0530

    floyd warshall

--- 1graph/floyd_warshall.cpp
+++ 1graph/floyd_warshall.cpp
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
 class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
@@ -33,6 +24,17 @@ class Solution {
     	        }
     	    }
 	    }
+
+        // //to check for -ve cycle 
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(matrix[i][i]<0)
+        //     {
+        //         cout<<"-ve cycle"<<endl;
+        //         break;
+        //     }
+        // }
+
 	    for(int i=0;i<n;i++)
 	    {
 	        for(int j=0;j<n;j++)
@@ -45,29 +47,3 @@ class Solution {
 	    }
 	}
 };
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>matrix(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cin >> matrix[i][j];
-			}
-		}
-		Solution obj;
-		obj.shortest_distance(matrix);
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cout << matrix[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
-}
-// } Driver Code Ends
