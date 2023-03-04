07025112b3b9584e9a4535e2afa3e45b433543ec
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2023-03-04 15:00:41 +0530

    medium

--- day9/palindromePartitioning.cpp
+++ day9/palindromePartitioning.cpp
@@ -1,51 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    bool partition(string s, int index,int i)
+    bool ispalindrome(string s)
     {
-        while(index<i)
-        {
-            if(s[index++]!=s[i--])
-                return false;
-        }
-        return true;
+        string temp=s;
+        reverse(s.begin(),s.end());
+        return s==temp;
     }
-    
-    void helper(int index,string s,vector<string> curr,vector<vector<string>> &ans)
+    void helper(int l,int r,vector<string> &curr,string &s,vector<vector<string>> &ans)
     {
-        //base case
-        if(index==s.size())
-        {
+        if(l==r ){
             ans.push_back(curr);
             return;
         }
-        
-        
-        for(int i=index;i<s.size();i++)
+        //partinoning
+        for(int i=l;i<r;i++)       
         {
-            //check for palindrome till i from index
-            if(partition(s,index,i))
+            if(ispalindrome(s.substr(l,i-l+1)))
             {
-                string ad="";
-                for(int k=index;k<=i;k++)
-                {
-                    ad+=s[k];
-                }
-                curr.push_back(ad);
-                helper(i+1,s,curr,ans);
+                curr.push_back(s.substr(l,i-l+1));
+                helper(i+1,r,curr,s,ans);
                 curr.pop_back();
             }
         }
+        return;
     }
-    
-    
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        
         vector<string> curr;
-        int index=0;
-        int partition=0;
-        helper(index,s,curr,ans);
-        
-        return ans;
+        helper(0,s.size(),curr,s,ans);       
+        return ans; 
     }
 };
+
+
